d12202cd701b18cfd75609e1e35845cadfdbbf33
DrLiveasy <youssef.saleh.hans@gmail.com>
2022-11-24 15:25:04 +0100

    creata la funzione StampaArrayInteri

--- CSharpFunctions/Program.cs
+++ CSharpFunctions/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// See https://aka.ms/new-console-template for more informationi
+
+
+
+
+
+
+
+
+
+
+
+
+//-------------------Funzioni-------------------
+
+void StampaArrayInteri(int[] arrayInteri)
+{
+    Console.Write("[");
+    for (int i = 0; i < arrayInteri.Length; i++)
+    {
+        if (i == arrayInteri.Length - 1)
+        {
+            Console.Write(arrayInteri[i]);
+        }
+        else
+        {
+            Console.Write(arrayInteri[i] + " , ");
+        }
+    }
+    Console.Write("]");
+
+}
